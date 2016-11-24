9f7582c549d0a5e13bebaa5da1f31054fed0839d
Marten Prieß <marten@non-rocket-science.com>
2016-11-24 11:08:42 +0100

    changed demo to spring-boot application

---
+++ vaadin-grid-util-demo/src/main/java/org/vaadin/gridutil/demo/Application.java
@@ -0,0 +1,16 @@
+package org.vaadin.gridutil.demo;
+
+
+import com.vaadin.spring.annotation.EnableVaadin;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableVaadin
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run((Application.class), args);
+    }
+}
+
