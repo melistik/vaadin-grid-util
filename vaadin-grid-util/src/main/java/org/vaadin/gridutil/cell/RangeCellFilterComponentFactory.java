7ac370d8ddf42f9d39904eecca713b50a16d4183
hitchhiker-jiri <georg@hicker.eu>
2017-08-03 16:30:28 +0200

    further typesafety and implementation

---
+++ vaadin-grid-util/src/main/java/org/vaadin/gridutil/cell/RangeCellFilterComponentFactory.java
@@ -0,0 +1,122 @@
+package org.vaadin.gridutil.cell;
+
+import com.vaadin.data.Converter;
+import com.vaadin.data.converter.*;
+import com.vaadin.server.SerializablePredicate;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import org.vaadin.gridutil.cell.filter.BetweenFilter;
+import org.vaadin.gridutil.cell.filter.EqualFilter;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+/**
+ * Created by georg.hicker on 03.08.2017.
+ */
+public class RangeCellFilterComponentFactory {
+
+    public static <T extends Number & Comparable<? super T>> RangeCellFilterComponentTyped<T, TextField, HorizontalLayout> createForNumberType(String propertyId, Class<T> propertyType, String converterErrorMessage, String smallestInputPrompt, String biggestInputPrompt, BiConsumer<SerializablePredicate<T>, String> filterReplaceConsumer, Consumer<String> filterRemoveConsumer) {
+        if (Integer.class.equals(propertyType)
+                || Long.class.equals(propertyType)
+                || Double.class.equals(propertyType)
+                || Float.class.equals(propertyType)
+                || BigInteger.class.equals(propertyType)
+                || BigDecimal.class.equals(propertyType)) {
+            return new RangeCellFilterComponentTyped<T, TextField, HorizontalLayout>() {
+                private TextField smallest, biggest;
+
+                @Override
+                public TextField getSmallestField() {
+                    if (smallest == null) {
+                        smallest = genNumberField(SMALLEST, getConverter(), smallestInputPrompt);
+                    }
+                    return smallest;
+                }
+
+                @Override
+                public TextField getBiggestField() {
+                    if (biggest == null) {
+                        biggest = genNumberField(BIGGEST, getConverter(), biggestInputPrompt);
+                    }
+                    return biggest;
+                }
+
+                private Converter getConverter() {
+                    if (Integer.class.equals(propertyType)) {
+                        return new StringToIntegerConverter(converterErrorMessage);
+                    } else if (Double.class.equals(propertyType)) {
+                        return new StringToDoubleConverter(converterErrorMessage);
+                    } else if (Float.class.equals(propertyType)) {
+                        return new StringToFloatConverter(converterErrorMessage);
+                    } else if (BigInteger.class.equals(propertyType)) {
+                        return new StringToBigIntegerConverter(converterErrorMessage);
+                    } else if (BigDecimal.class.equals(propertyType)) {
+                        return new StringToBigDecimalConverter(converterErrorMessage);
+                    } else {
+                        return new StringToLongConverter(converterErrorMessage);
+                    }
+                }
+
+                private TextField genNumberField(final String propertyId, final Converter converter, final String inputPrompt) {
+                    return FieldFactory.genNumberField(() -> getBinder(), propertyId, converter, inputPrompt);
+                }
+
+                @Override
+                public HorizontalLayout layoutComponent() {
+                    getHLayout().addComponent(getSmallestField());
+                    getHLayout().addComponent(getBiggestField());
+                    getHLayout().setExpandRatio(getSmallestField(), 1);
+                    getHLayout().setExpandRatio(getBiggestField(), 1);
+
+                    initBinderValueChangeHandler();
+
+                    return getHLayout();
+                }
+
+                private void initBinderValueChangeHandler() {
+                    getBinder().addValueChangeListener(e -> {
+                        Object smallest = getBinder().getBean()
+                                .getSmallest();
+                        Object biggest = getBinder().getBean()
+                                .getBiggest();
+                        if (smallest != null || biggest != null) {
+                            final T smallestValue = checkObject(smallest);
+                            final T biggestValue = checkObject(biggest);
+                            if (smallest != null && biggest != null && smallest.equals(biggest)) {
+                                filterReplaceConsumer.accept(
+                                        new EqualFilter(smallestValue),
+                                        propertyId);
+                            } else {
+                                filterReplaceConsumer.accept(
+                                        new BetweenFilter(
+                                                (smallestValue != null ? smallestValue : NumberUtil.getBoundaryValue(propertyType, false)),
+                                                (biggestValue != null ? biggestValue : NumberUtil.getBoundaryValue(propertyType, true))
+                                        ),
+                                        propertyId);
+                            }
+                        } else {
+                            filterRemoveConsumer.accept(propertyId);
+                        }
+
+                    });
+                }
+
+                private T checkObject(Object value) {
+                    if (value != null && value.getClass().equals(propertyType)) {
+                        return propertyType.cast(value);
+                    }
+                    return null;
+                }
+
+                @Override
+                public void clearFilter() {
+                    getBinder().setBean(new TwoValueObjectTyped<T>());
+                }
+            };
+        }
+        return null;
+    }
+}
---
+++ vaadin-grid-util/src/main/java/org/vaadin/gridutil/cell/RangeCellFilterComponentTyped.java
@@ -0,0 +1,58 @@
+package org.vaadin.gridutil.cell;
+
+import com.vaadin.data.Binder;
+import com.vaadin.data.HasValue;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
+
+/**
+ * extends CellFilterComponent to allow smallest and biggest filter Component
+ *
+ * @author Marten Prieß (http://www.rocketbase.io)
+ * @version 1.1
+ */
+public abstract class RangeCellFilterComponentTyped<T, F extends HasValue, C extends Component> extends CellFilterComponent<C> {
+
+    public static final String SMALLEST = "smallest";
+    public static final String BIGGEST = "biggest";
+    private static final long serialVersionUID = 1L;
+    private HorizontalLayout hLayout;
+    private Binder<TwoValueObjectTyped<T>> binder;
+
+    public abstract F getSmallestField();
+
+    public abstract F getBiggestField();
+
+    /**
+     * creates the layout when not already done
+     *
+     * @return a HLayout with already set style
+     */
+    public HorizontalLayout getHLayout() {
+        if (this.hLayout == null) {
+            this.hLayout = new HorizontalLayout();
+            this.hLayout.setMargin(false);
+            this.hLayout.addStyleName("filter-header");
+        }
+        return this.hLayout;
+    }
+
+    /**
+     * create binder when not already done
+     *
+     * @return instance of binder
+     */
+    public Binder<TwoValueObjectTyped<T>> getBinder() {
+        if (this.binder == null) {
+            this.binder = new Binder(TwoValueObjectTyped.class);
+            this.binder.setBean(new TwoValueObjectTyped<>());
+        }
+        return this.binder;
+    }
+
+    @Override
+    public void triggerUpdate() {
+        // trigger value Changed
+        getBinder().setBean(getBinder().getBean());
+    }
+}
