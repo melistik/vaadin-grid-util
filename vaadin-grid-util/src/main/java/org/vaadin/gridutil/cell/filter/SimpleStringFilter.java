8466fd5b4a615032a3966e52a58ae6f148ee55cd
Marten Prieß <marten@rocketbase.io>
2018-03-07 16:46:19 +0100

    Revert "Add support for generic data provider, and more."

--- vaadin-grid-util/src/main/java/org/vaadin/gridutil/cell/filter/SimpleStringFilter.java
+++ vaadin-grid-util/src/main/java/org/vaadin/gridutil/cell/filter/SimpleStringFilter.java
@@ -1,21 +1,41 @@
 package org.vaadin.gridutil.cell.filter;
 
+import com.vaadin.server.SerializablePredicate;
+
 /**
  * Created by marten on 22.02.17.
  */
-public class SimpleStringFilter extends EqualFilter<String> {
+public class SimpleStringFilter implements SerializablePredicate<String> {
 
-    public final boolean ignoreCase;
-    public final boolean onlyMatchPrefix;
+    final String filterString;
+    final boolean ignoreCase;
+    final boolean onlyMatchPrefix;
 
-    public SimpleStringFilter(String columnId, String filterString, boolean ignoreCase, boolean onlyMatchPrefix) {
-        super(columnId, filterString);
+    public SimpleStringFilter(String filterString, boolean ignoreCase, boolean onlyMatchPrefix) {
         this.ignoreCase = ignoreCase;
+        // ignoreCase has to be applied to filterstring too, otherwise uppercase input won't work
+        this.filterString = this.ignoreCase ? filterString.toLowerCase() : filterString;
         this.onlyMatchPrefix = onlyMatchPrefix;
     }
 
     @Override
-    public String toString() {
-        return "SimpleStringFilter{columnId='" + columnId + '\'' + "ignoreCase=" + ignoreCase + ", onlyMatchPrefix=" + onlyMatchPrefix + ", toCompare=" + toCompare + '}';
+    public boolean test(String value) {
+        if (filterString == null || value == null) {
+            return false;
+        }
+        final String v = ignoreCase ? value.toString()
+                .toLowerCase()
+                : value.toString();
+
+        if (onlyMatchPrefix) {
+            if (!v.startsWith(filterString)) {
+                return false;
+            }
+        } else {
+            if (!v.contains(filterString)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
